b1648131ea918fca1b5ef7c88baa9b58a7db4817
beingdevelop <rocko2494@gmail.com>
2018-03-07 16:12:30 +0600

    connection mysql

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "I am the best by";
-echo "Now what are you doing";
+
+$servername = "localhost";
+$username = "rokon";
+$password = "blood@1994";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+/*echo "<pre>";
+print_r($conn);
+exit();*/
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
